4e393c1c045e51ff482b80a47297deb064e6829e
Leeco <leeco19@qq.com>
2024-03-28 04:27:52 +0000

    feat: use stylex, stylex-webpack

--- apps/stylex-labs/app/page.tsx
+++ apps/stylex-labs/app/page.tsx
@@ -1,3 +1,15 @@
+import * as stylex from '@stylexjs/stylex';
+
+const styles = stylex.create({
+  button: {
+    backgroundColor: {
+      default: 'lightblue',
+      ':hover': 'blue',
+      ':active': 'darkblue',
+    },
+  },
+});
+
 export default function Index() {
   /*
    * Replace the elements below with your own.
@@ -8,6 +20,7 @@ export default function Index() {
     <div className="wrapper">
       <div className="container">
         <h1 className="text-yellow-500 text-center">Stylex Labs</h1>
+        <button {...stylex.props(styles.button)}>Active</button>
       </div>
     </div>
   );
